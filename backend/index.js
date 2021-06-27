02d45251a2058894f9c1d47e449d5a2ae42a8acb
Neuldz <irene@arrowhitech.com>
2021-06-27 10:34:00 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,30 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
-import posts from './routers/posts.js' 
+import posts from './routers/posts.js' ;
+import mongoose from 'mongoose';
+
 
 const app = express();
 const PORT = process.env.port || 5000;
+
+
+const URI = 'mongodb+srv://admin:admin/ziLH7K4hrAHADuv@cluster0.4fx0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 
 app.use(bodyParser.json({limit: '30mb'}));
 app.use(bodyParser.urlencoded({extended: true,limit: '30mb'}));
 app.use(cors());
 
 app.use('/posts',posts);
+
+mongoose.connect(URI, {useNewUrlParser: true, useUnfiedTopology: true})
+.then(()=>{
+
+	console.log('connected to db');	
+}).catch((err)=>{
+	console.log('err',errr);
+})
 
 app.listen(PORT, () => {
 	console.log(`App is running :v on PORT : ${PORT}`);
