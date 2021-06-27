497dd10c2d5d0183916173f12c2dd9269b570e64
Neuldz <irene@arrowhitech.com>
2021-06-27 16:53:58 +0700

    model

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.port || 5000;
 
 
-const URI = 'mongodb+srv://admin:admin/ziLH7K4hrAHADuv@cluster0.4fx0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://admin:ziLH7K4hrAHADuv@cluster0.4fx0c.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
 
 app.use(bodyParser.json({limit: '30mb'}));
@@ -18,12 +18,12 @@ app.use(cors());
 
 app.use('/posts',posts);
 
-mongoose.connect(URI, {useNewUrlParser: true, useUnfiedTopology: true})
+mongoose.connect(URI, {useNewUrlParser: true, useUnifiedTopology: true})
 .then(()=>{
 
 	console.log('connected to db');	
 }).catch((err)=>{
-	console.log('err',errr);
+	console.log('err',err);
 })
 
 app.listen(PORT, () => {
